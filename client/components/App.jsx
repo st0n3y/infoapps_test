dc58e499635bde393c0babbc7f49fa43bc4d21ea
David MacKintosh <dgawmackintosh@gmail.com>
2017-02-26 10:38:27 +0000

    Refactors to make CheckoutForm skinny

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -10,12 +10,64 @@ export default class App extends Component {
       success: false,
     };
     this.performSubmit = this.performSubmit.bind(this);
+    this.handlePostcode = this.handlePostcode.bind(this);
+    this.requestAddressFromAPI = this.requestAddressFromAPI.bind(this);
+    this.autofillFields = this.autofillFields.bind(this);
   }
   
   performSubmit() {
     this.setState({
       success: true
     })
+  }
+
+  handlePostcode(postcode) {
+    var postcodeArray = postcode.split(" ");
+    var postcode1 = postcodeArray[0];
+    var postcode2 = postcodeArray[1];
+      
+    this.requestAddressFromAPI(postcode1, postcode2)
+      .then((parsedData) => {
+        this.autofillFields(parsedData);
+      })
+  }
+
+  requestAddressFromAPI(postcode1, postcode2) {
+    var url = "https://maps.googleapis.com/maps/api/geocode/json?address=";
+    const key = "&key=AIzaSyDUjs1SZNoF_J70CSSvVvIVCW13qXk_06s";
+    var request = new XMLHttpRequest();
+    
+    url = url + postcode1 + "+" + postcode2 + key;
+
+    return new Promise((resolve, reject) => {
+      request.open("GET", url);
+      request.setRequestHeader("Content-Type", "application/json");
+      request.onload = () => {
+        if(request.status !== 200) {
+          reject("Error retrieving data")
+        } else {
+          var parsedData = JSON.parse(request.responseText);
+          resolve(parsedData);
+        }
+      }
+      request.send(null);
+    });
+  }
+
+  autofillFields(parsedData) {
+    var longitude = parsedData.results[0].geometry.location.lng;
+    var latitude = parsedData.results[0].geometry.location.lat;
+
+    var addressObject = parsedData.results[0].address_components;
+    var addressValue = addressObject[1].long_name;
+    var cityValue = addressObject[2].long_name;
+    var countryValue = addressObject[4].long_name;
+
+    document.getElementById("address").value = addressValue;
+    document.getElementById("city").value = cityValue;
+    document.getElementById("country").value = countryValue;
+    document.getElementById("longitude").value = longitude;
+    document.getElementById("latitude").value = latitude;
   }
 
   render() {
@@ -29,7 +81,10 @@ export default class App extends Component {
     } else {
       return(
         <div className="outer-container">
-          <CheckoutForm onSubmit={this.performSubmit} url="https://maps.googleapis.com/maps/api/geocode/json?address="/>
+          <CheckoutForm 
+            onSubmit={this.performSubmit} 
+            onPostcodeEntry={this.handlePostcode} 
+          />
         </div>
       );
     }
--- client/components/CheckoutForm.jsx
+++ client/components/CheckoutForm.jsx
@@ -1,6 +1,3 @@
-// API key: AIzaSyDUjs1SZNoF_J70CSSvVvIVCW13qXk_06s
-// Sample API request: https://maps.googleapis.com/maps/api/geocode/json?
-
 import React, { Component } from 'react';
 import SuccessMessage from './SuccessMessage.jsx'
 import classNames from 'classnames';
@@ -11,63 +8,17 @@ export default class CheckoutForm extends Component {
     super();
     this.state = {
       okToSubmit: true,
-      // data: [],
     };
     this.handleSubmit = this.handleSubmit.bind(this);
     this.getPostcode = this.getPostcode.bind(this);
-    this.requestAddressFromAPI = this.requestAddressFromAPI.bind(this);
   }
 
   getPostcode() {
     var postcode = document.getElementById("postcode").value;
     
-    var postcodeArray = postcode.split(" ");
-    var postcode1 = postcodeArray[0];
-    var postcode2 = postcodeArray[1];
-    
-    this.requestAddressFromAPI(postcode1, postcode2)
-      .then((parsedData) => {
-        this.autofillFields(parsedData);
-      })
-  }
-
-  requestAddressFromAPI(postcode1, postcode2) {
-    var url = this.props.url;
-    var key = "&key=AIzaSyDUjs1SZNoF_J70CSSvVvIVCW13qXk_06s";
-    var request = new XMLHttpRequest();
-    
-    url = url + postcode1 + "+" + postcode2 + key;
-
-    return new Promise((resolve, reject) => {
-      request.open("GET", url);
-      request.setRequestHeader("Content-Type", "application/json");
-      request.onload = () => {
-        if(request.status !== 200) {
-          reject("Error retrieving data")
-        } else {
-          var parsedData = JSON.parse(request.responseText);
-          // this.setState({ data: data });
-          resolve(parsedData);
-        }
-      }
-      request.send(null);
-    });
-  }
-
-  autofillFields(parsedData) {
-    var longitude = parsedData.results[0].geometry.location.lng;
-    var latitude = parsedData.results[0].geometry.location.lat;
-
-    var addressObject = parsedData.results[0].address_components;
-    var addressValue = addressObject[1].long_name;
-    var cityValue = addressObject[2].long_name;
-    var countryValue = addressObject[4].long_name;
-
-    document.getElementById("address").value = addressValue;
-    document.getElementById("city").value = cityValue;
-    document.getElementById("country").value = countryValue;
-    document.getElementById("longitude").value = longitude;
-    document.getElementById("latitude").value = latitude;
+    if(postcode !== "") {
+      this.props.onPostcodeEntry(postcode);
+    }
   }
 
   handleSubmit(e) {
